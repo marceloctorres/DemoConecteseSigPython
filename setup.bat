76fde36ad7eafa5b3a11b57bc84a49fc47223f79
Marcelo César Torres <mt@marceloctorres.net>
2022-05-26 12:23:54 -0500

    Actualizacion URL Home

--- setup.bat
+++ setup.bat
@@ -4,4 +4,4 @@ cd ..
 python -m pip install --upgrade pip setuptools wheel build twine
 python setup.py sdist bdist_wheel
 
-# python -m twine upload --verbose --config-file .pypirc --repository publico dist/*
+python -m twine upload --verbose --config-file .pypirc --repository publico dist/*
--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()    
  
 setuptools.setup(name='DemoConecteseSigPy', 
-        version='0.0.0.1',
+        version='0.0.0.2',
         author='Esri Colombia',
         author_email='mtorres@esri.co',
         license='MIT',
         long_description=long_description,
         long_description_content_type="text/markdown",
-        url='https://dev.azure.com/esrico-con-uaecd-ladm/ImportarEncuestaActualizacionCatastral',
+        url='https://github.com/marceloctorres/DemoConecteseSigPython',
         description=('Herramientas Demostración para el Conectese con SIG - ArcGIS Pro.'),
         python_requires= '>=3.6.10',
         classifiers=[
